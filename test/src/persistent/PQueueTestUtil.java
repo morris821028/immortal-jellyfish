9b361da682df548a74361b8a2bf443295d677588
Shiang-Yun Yang <morris821028@gmail.com>
2019-12-25 08:58:06 +0800

    PQueue: add sliding test case

--- test/src/persistent/PQueueTestUtil.java
+++ test/src/persistent/PQueueTestUtil.java
@@ -5,7 +5,26 @@ import org.junit.jupiter.api.Assertions;
 import persistent.PQueue;
 
 public class PQueueTestUtil {
-	private PQueueTestUtil() {
+	private PQueueTestUtil() {}
+
+	public static void testQueueAsSliding(final PQueue<Integer> empty) {
+		PQueue<Integer> que = empty;
+
+		int m = 100000;
+		int n = 1000000;
+		for (int i = 0; i < m; i++) {
+			que = que.push(i);
+			Assertions.assertEquals(i + 1L, que.size());
+		}
+
+		for (int i = m; i < n; i++) {
+			Integer v = que.front();
+			Assertions.assertEquals(i - m, v);
+			que = que.pop();
+			que = que.push(i);
+			Assertions.assertEquals(m, que.size());
+		}
+		System.out.println("testQueueAsSliding() pass");
 	}
 
 	public static void testQueue(PQueue<Integer> empty) {
@@ -43,5 +62,6 @@ public class PQueueTestUtil {
 			Assertions.assertEquals(n - i - 1, que.size());
 		}
 		System.out.println("testQueue() pass");
+		testQueueAsSliding(empty);
 	}
 }
