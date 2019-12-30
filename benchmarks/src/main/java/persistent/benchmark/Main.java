2bea491d9d16726206b071312665b94f93d21a7c
Shiang-Yun Yang <morris821028@gmail.com>
2019-12-30 20:00:31 +0800

    Benchmark: add options

--- benchmarks/src/main/java/persistent/benchmark/Main.java
+++ benchmarks/src/main/java/persistent/benchmark/Main.java
@@ -1,6 +1,7 @@
 package persistent.benchmark;
 
 import org.openjdk.jmh.profile.GCProfiler;
+import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -11,8 +12,12 @@ public class Main {
 	public static void main(String[] args) {
 
 		Options options = new OptionsBuilder().include(QueueBenchmark.class.getSimpleName())
-				.warmupTime(TimeValue.milliseconds(100)).measurementTime(TimeValue.seconds(1))
-				.addProfiler(GCProfiler.class).forks(1).build();
+				.warmupTime(TimeValue.milliseconds(100)) //
+				.measurementTime(TimeValue.seconds(1)) //
+				.result("result.json") //
+				.resultFormat(ResultFormatType.JSON) //
+				// .addProfiler(GCProfiler.class) // garbage collection profiler
+				.forks(1).build();
 		try {
 			new Runner(options).run();
 		} catch (RunnerException e) {
