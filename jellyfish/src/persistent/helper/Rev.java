16410ebf90cb19754be6c59fa675bf3227bd95da
Shiang-Yun Yang <morris821028@gmail.com>
2019-12-31 17:07:15 +0800

    Rev: add another option for reverse list(stack)

--- jellyfish/src/persistent/helper/Rev.java
+++ jellyfish/src/persistent/helper/Rev.java
@@ -1,7 +1,7 @@
 package persistent.helper;
 
 import persistent.PStack;
-import persistent.stack.PersistStack;
+import persistent.util.PCollections;
 
 public class Rev<T> extends PStack<T> {
 	private final PStack<T> x;
@@ -40,29 +40,59 @@ public class Rev<T> extends PStack<T> {
 		pop = getReal().pop();
 		return pop;
 	}
-	
+
 	private PStack<T> getReal() {
 		if (y != null)
 			return y;
-		y = PersistStack.create();
-		PStack<T> tmp = x;
-		while (!tmp.isEmpty()) {
-			y = y.push(tmp.top());
-			tmp = tmp.pop();
-		}
+		y = reverse(x);
 		assert y.size() == x.size() : String.format("%d %d %s", y.size(), x.size(), x.getClass());
 		return y;
 	}
 
+	/**
+	 * <p>
+	 * Flatten, and then reverse stack.
+	 * </p>
+	 * <p>
+	 * Time complexity O(|x|)
+	 * </p>
+	 * 
+	 * @param <T> the type of elements
+	 * @param x   stack
+	 * @return A reversed stack
+	 * @see #create(PStack)
+	 */
+	public static <T> PStack<T> reverse(PStack<T> x) {
+		PStack<T> y = PCollections.emptyStack();
+		while (!x.isEmpty()) {
+			y = y.push(x.top());
+			x = x.pop();
+		}
+		return y;
+	}
+
+	/**
+	 * <p>
+	 * Create a lazy evaluation reverse stack
+	 * </p>
+	 * <p>
+	 * Invariant {@literal |x| <= 6} in internal use.
+	 * </p>
+	 * 
+	 * @param <T> the type of elements
+	 * @param r   stack
+	 * @return A lazy revered stack
+	 */
 	public static <T> PStack<T> create(PStack<T> r) {
 		if (r.isEmpty())
-			return PersistStack.create();
+			return PCollections.emptyStack();
 		if (r.size() == 1)
 			return r;
 		if (r instanceof Rev<?>) {
 			Rev<T> t = (Rev<T>) r;
 			return t.x;
 		}
+		assert r.size() <= 6 : String.format("r size = %d", r.size());
 		return new Rev<>(r);
 	}
 }
