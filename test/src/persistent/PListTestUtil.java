e4f5c588359d0232dae135b52e5a4639594881b8
Shiang-Yun Yang <morris821028@gmail.com>
2020-01-04 11:35:15 +0800

    TreeList: move test to common utilities

---
+++ test/src/persistent/PListTestUtil.java
@@ -0,0 +1,128 @@
+package persistent;
+
+import org.junit.jupiter.api.Assertions;
+
+public class PListTestUtil {
+	private PListTestUtil() {
+	}
+
+	public static void testPushBackGet(PList<Integer> empty) {
+		PList<Integer> a = empty;
+
+		for (int i = 0; i < 32; i++) {
+			a = a.pushBack(i);
+			for (int j = 0; j <= i; j++)
+				Assertions.assertEquals(j, a.get(j));
+		}
+
+		a = empty;
+		for (int i = 0; i < 1000000; i++) {
+			a = a.pushBack(i);
+		}
+
+		for (int i = 0; i < a.size(); i++)
+			Assertions.assertEquals(i, a.get(i));
+	}
+
+	public static void testPopBackGet(PList<Integer> empty) {
+		PList<Integer> a = empty;
+
+		for (int i = 0; i < 32; i++) {
+			a = a.pushBack(i);
+		}
+
+		for (int i = 31; i >= 0; i--) {
+			a = a.popBack();
+			for (int j = 0; j < i; j++)
+				Assertions.assertEquals(j, a.get(j));
+		}
+
+		a = empty;
+		for (int i = 0; i < 1000000; i++) {
+			a = a.pushBack(i);
+		}
+
+		for (int i = 0; i < 1000000; i++) {
+			a = a.popBack();
+		}
+	}
+
+	public static void testPushBackSet(PList<Integer> empty) {
+		PList<Integer> a = empty;
+		int[] fib = new int[32];
+		fib[0] = 1;
+		fib[1] = 1;
+		for (int i = 2; i < 32; i++) {
+			fib[i] = fib[i - 1] + fib[i - 2];
+		}
+
+		for (int i = 0; i < 32; i++)
+			a = a.pushBack(null);
+
+		a = a.set(0, 1);
+		Assertions.assertEquals(1, a.get(0));
+		a = a.set(1, 1);
+		Assertions.assertEquals(1, a.get(0));
+		Assertions.assertEquals(1, a.get(1));
+		for (int i = 2; i < 20; i++) {
+			a = a.set(i, a.get(i - 1) + a.get(i - 2));
+			for (int j = 0; j <= i; j++)
+				Assertions.assertEquals(fib[j], a.get(j), "" + j);
+		}
+
+		a = empty;
+		int n = 1000000;
+		for (int i = 0; i < 1000000; i++) {
+			a = a.pushBack(i);
+		}
+
+		for (int i = 0; i < n; i++)
+			a = a.set(i, n - i);
+
+		for (int i = 0; i < a.size(); i++)
+			Assertions.assertEquals(n - i, a.get(i));
+	}
+
+	public static void testToString(PList<Object> empty) {
+		PList<Object> a = empty;
+		a = a.pushBack(3);
+		a = a.pushBack(".");
+		a = a.pushBack(1);
+		a = a.pushBack(4);
+
+		Assertions.assertEquals("{size=4, [3, ., 1, 4]}", a.toString());
+	}
+
+	public static void testDemo(PList<Integer> empty) {
+		PList<Integer> a = empty;
+		for (int i = 0; i < 10; i++)
+			a = a.pushBack(null);
+		a = a.set(0, 1);
+		a = a.set(1, 1);
+		for (int i = 2; i < 10; i++)
+			a = a.set(i, a.get(i - 1) + a.get(i - 2));
+		Assertions.assertEquals("{size=10, [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]}", a.toString());
+		a = a.popBack();
+		Assertions.assertEquals("{size=9, [1, 1, 2, 3, 5, 8, 13, 21, 34]}", a.toString());
+	}
+
+	public static void testZigZag(PList<Integer> empty) {
+		PList<Integer> a = empty;
+		a = a.pushBack(0);
+		a = a.pushBack(1);
+		for (int i = 0; i < 200000; i++) {
+			for (int j = 2; j < 32; j++)
+				a = a.pushBack(j);
+			for (int j = 2; j < 32; j++)
+				a = a.popBack();
+		}
+		Assertions.assertEquals(a.size(), 2);
+		for (int i = 0; i < 1000000; i++) {
+			Assertions.assertNotNull(a.popBack());
+		}
+		for (int i = 0; i < 1000000; i++) {
+			Assertions.assertEquals(a.get(0), 0);
+			Assertions.assertEquals(a.get(1), 1);
+		}
+	}
+}
--- test/src/persistent/array/TreeListTest.java
+++ test/src/persistent/array/TreeListTest.java
@@ -1,135 +1,37 @@
 package persistent.array;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import persistent.PList;
-import persistent.util.PCollections;
+import persistent.PListTestUtil;
 
 public class TreeListTest {
 	@Test
 	public void testPushBackGet() {
-		PList<Integer> a = TreeList.create();
-
-		for (int i = 0; i < 32; i++) {
-			a = a.pushBack(i);
-			for (int j = 0; j <= i; j++)
-				Assertions.assertEquals(j, a.get(j));
-		}
-
-		a = TreeList.create();
-		for (int i = 0; i < 1000000; i++) {
-			a = a.pushBack(i);
-		}
-
-		for (int i = 0; i < a.size(); i++)
-			Assertions.assertEquals(i, a.get(i));
+		PListTestUtil.testPushBackGet(TreeList.create());
 	}
 
 	@Test
 	public void testPopBackGet() {
-		PList<Integer> a = TreeList.create();
-
-		for (int i = 0; i < 32; i++) {
-			a = a.pushBack(i);
-		}
-
-		for (int i = 31; i >= 0; i--) {
-			a = a.popBack();
-			for (int j = 0; j < i; j++)
-				Assertions.assertEquals(j, a.get(j));
-		}
-
-		a = TreeList.create();
-		for (int i = 0; i < 1000000; i++) {
-			a = a.pushBack(i);
-		}
-
-		for (int i = 0; i < 1000000; i++) {
-			a = a.popBack();
-		}
+		PListTestUtil.testPopBackGet(TreeList.create());
 	}
 
 	@Test
 	public void testPushBackSet() {
-		PList<Integer> a = TreeList.create();
-		int[] fib = new int[32];
-		fib[0] = 1;
-		fib[1] = 1;
-		for (int i = 2; i < 32; i++) {
-			fib[i] = fib[i - 1] + fib[i - 2];
-		}
-
-		for (int i = 0; i < 32; i++)
-			a = a.pushBack(null);
-
-		a = a.set(0, 1);
-		Assertions.assertEquals(1, a.get(0));
-		a = a.set(1, 1);
-		Assertions.assertEquals(1, a.get(0));
-		Assertions.assertEquals(1, a.get(1));
-		for (int i = 2; i < 20; i++) {
-			a = a.set(i, a.get(i - 1) + a.get(i - 2));
-			for (int j = 0; j <= i; j++)
-				Assertions.assertEquals(fib[j], a.get(j), "" + j);
-		}
-
-		a = TreeList.create();
-		int n = 1000000;
-		for (int i = 0; i < 1000000; i++) {
-			a = a.pushBack(i);
-		}
-
-		for (int i = 0; i < n; i++)
-			a = a.set(i, n - i);
-
-		for (int i = 0; i < a.size(); i++)
-			Assertions.assertEquals(n - i, a.get(i));
+		PListTestUtil.testPushBackSet(TreeList.create());
 	}
 
 	@Test
 	public void testToString() {
-		PList<Object> a = TreeList.create();
-		a = a.pushBack(3);
-		a = a.pushBack(".");
-		a = a.pushBack(1);
-		a = a.pushBack(4);
-
-		Assertions.assertEquals("{size=4, [3, ., 1, 4]}", a.toString());
+		PListTestUtil.testToString(TreeList.create());
 	}
 
 	@Test
 	public void testDemo() {
-		PList<Integer> a = PCollections.emptyList();
-		for (int i = 0; i < 10; i++)
-			a = a.pushBack(null);
-		a = a.set(0, 1);
-		a = a.set(1, 1);
-		for (int i = 2; i < 10; i++)
-			a = a.set(i, a.get(i - 1) + a.get(i - 2));
-		Assertions.assertEquals("{size=10, [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]}", a.toString());
-		a = a.popBack();
-		Assertions.assertEquals("{size=9, [1, 1, 2, 3, 5, 8, 13, 21, 34]}", a.toString());
+		PListTestUtil.testDemo(TreeList.create());
 	}
 
 	@Test
 	public void testZigZag() {
-		PList<Integer> a = TreeList.create();
-		a = a.pushBack(0);
-		a = a.pushBack(1);
-		for (int i = 0; i < 200000; i++) {
-			for (int j = 2; j < 32; j++)
-				a = a.pushBack(j);
-			for (int j = 2; j < 32; j++)
-				a = a.popBack();
-		}
-		Assertions.assertEquals(a.size(), 2);
-		for (int i = 0; i < 1000000; i++) {
-			Assertions.assertNotNull(a.popBack());
-		}
-		for (int i = 0; i < 1000000; i++) {
-			Assertions.assertEquals(a.get(0), 0);
-			Assertions.assertEquals(a.get(1), 1);
-		}
+		PListTestUtil.testZigZag(TreeList.create());
 	}
 }
